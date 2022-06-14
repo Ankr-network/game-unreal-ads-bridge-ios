35b462409316458fe5883eaadd7fa604c092fb31
ektishafahwaz <ektishaf@ankr.com>
2022-06-14 14:28:28 +0400

    Package Updated
    - Package is now included via github repo
    - Scripts updated for AnkrAds.swift

--- AnkrAds/AnkrAds/AnkrAds.swift
+++ AnkrAds/AnkrAds/AnkrAds.swift
@@ -8,33 +8,35 @@
 import ANKR_adlibrary
 import SwiftUI
 
-@objc open class AnkrAds : NSObject {
+@objc open class AnkrAds : NSObject
+{
     var service : AdService
 
     @objc public override init()
     {
-        service = AdServiceImp()
+        service = AdService()
         super.init()
         
-        print("AnkrAds - Initialized")
+        print("iOS - AnkrAds - Initialized")
     }
     
     @objc public func StartSession(appId : String, deviceId : String, publicAddress : String, language : String)
     {
-        print("AnkrAds - StartSession - appId: " + appId + " | deviceId: " + deviceId + " | publicAddress: " + publicAddress + " | language: " + language)
+        print("iOS - AnkrAds - StartSession - appId: " + appId + " | deviceId: " + deviceId + " | publicAddress: " + publicAddress + " | language: " + language)
+
         
-        service.start(startModel: StartModel(appId: appId, deviceId: deviceId, publicAddress: publicAddress, language: language))
+        service.start(appId: appId, deviceId: deviceId, publicAddress: publicAddress, language: language)
     }
     
     @objc public func LoadAd(unitId : String)
     {
-        print("AnkrAds - LoadAd - unitId: " + unitId)
+        print("iOS - AnkrAds - LoadAd - unitId: " + unitId)
         service.preloadAd(unitId: unitId)
     }
     
     @objc public func Show(view : UIView, unitId : String)
     {
-        print("AnkrAds - Show - unitId: " + unitId)
+        print("iOS - AnkrAds - Show - unitId: " + unitId)
         
         service.show(for: view, unitId: unitId)
     }
